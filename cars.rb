5e5239ac032b414678e776dc28fcaecc85c0d23b
MakerSquare Student <students@themakersquare.com>
2013-09-10 12:04:31 -0500

    Break for lunch

--- cars.rb
+++ cars.rb
@@ -1,11 +1,61 @@
 class Car
+	@@total_car_count = 0
+	@@cars_per_color = {}
+
 	def to_s()
 		"I'm a car! I've driven #{@distance} and have #{@fuel} gallons gas left"
 	end
 
-	def initialize()
+	# Returns total_car_count.
+	def self.total_car_count
+		@@total_car_count
+	end
+
+	# Returns car color that is most popular
+	def self.most_popular_color
+		most_pop_color = [] # Temporary variable for block
+		most_pop_color_count = 0
+		p @@cars_per_color
+
+		@@cars_per_color.each { |color, count|
+			if count > most_pop_color_count
+				most_pop_color_count = count
+				most_pop_color = [color]
+			end
+		}
+
+		p most_pop_color_count
+
+		@@cars_per_color.each_pair { |color, count|
+			if count == most_pop_color_count
+				most_pop_color.push(color)
+			end
+		}
+
+		return most_pop_color
+	end	
+
+
+	def initialize(new_car_color) # this is not counting correctly thank.
 		@fuel = 10
 		@distance = 0
+		@@total_car_count += 1
+		have_seen = false
+
+		if @@cars_per_color == {}
+			@@cars_per_color[new_car_color] = 1
+		else
+			@@cars_per_color.each_key {|color|
+				if color == new_car_color
+					@@cars_per_color[new_car_color] += 1
+				else
+					have_seen = true
+				end
+			}
+			@@cars_per_color[new_car_color] = 1 if have_seen
+		end
+
+		@car_color = new_car_color.capitalize
 	end
 
 	def drive(miles)
@@ -24,10 +74,32 @@ class Car
 		puts "You must pay $#{3.5 * gallons_needed}"
 		@fuel = 10.0
 	end
+
+# Enables car onwers to change the paint of their car and updates
+# Car class database.
+
+	def paint_car(new_paint_color)
+		new_paint_color.captialize!
+
+		@@cars_per_color.each_key { |color| 
+			if color == new_paint_color
+				@@cars_per_color[color] += 1
+			else
+				@@cars_per_color[new_paint_color] = 1	
+			end
+		}
+		
+		@@cars_per_color[@car_color] -= 1
+		@car_color = new_paint_color
+	end
 end
 
-car_a = Car.new()
-car_b = Car.new()
+car_a = Car.new("Green")
+car_b = Car.new("Red")
+car_c = Car.new("Blue")
+car_d = Car.new("Blue")
+car_e = Car.new("Blue")
+car_f = Car.new("Red")
 puts car_a
 puts car_b
 car_a.drive(10)
